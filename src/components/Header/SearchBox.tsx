f92fa21233665d9d72901c1f2d0044eb698af10f
LucasFlaquer <lucas.flaquer@gmail.com>
2021-06-16 00:20:23 -0300

    create component header

---
+++ src/components/Header/SearchBox.tsx
@@ -0,0 +1,30 @@
+import { Flex, Input, Icon } from '@chakra-ui/react';
+import { RiSearchLine } from 'react-icons/ri';
+
+export function SearchBox() {
+  return (
+    <Flex
+      as="label"
+      flex="1"
+      py="3"
+      px="8"
+      ml="6"
+      maxWidth={400}
+      alignSelf="center"
+      color="gray.600"
+      position="relative"
+      bg="blue.100"
+      borderRadius="full"
+    >
+      <Input
+        color="gray.600"
+        variant="unstyled"
+        px="4"
+        mr="4"
+        placeholder="Pesquisar pacientes, procedimentos..."
+        _placeholder={{ color: 'gray.200' }}
+      />
+      <Icon as={RiSearchLine} />
+    </Flex>
+  );
+}
