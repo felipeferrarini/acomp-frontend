ffe0ab7055abcdaabbe08d15e4d68173d86e6439
LucasFlaquer <lucas.flaquer@gmail.com>
2021-06-16 00:23:51 -0300

    created dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react';
+import { BaseTemplate } from '../components/Templates/BaseLayout';
+
+export default function Dashboard() {
+  return (
+    <BaseTemplate>
+      <Box />
+    </BaseTemplate>
+  );
+}
