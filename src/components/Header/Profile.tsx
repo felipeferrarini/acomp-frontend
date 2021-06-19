00ff907e2ad8ac6fe185e6c19ca3adcd6ac3e8c1
LucasFlaquer <lucas.flaquer@gmail.com>
2021-06-19 17:06:09 -0300

    menuButton profile actions

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,44 @@
-import { Flex, Box, Text, Avatar, Icon } from '@chakra-ui/react';
+import {
+  Flex,
+  Box,
+  Text,
+  Avatar,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  Icon,
+} from '@chakra-ui/react';
 import { FaChevronDown } from 'react-icons/fa';
+import { useAuthContext } from '../../contexts/AuthContex';
 
 export function Profile() {
+  const { user, signOut } = useAuthContext();
+
   return (
-    <Flex align="center">
+    <Flex align="center" position="relative">
       <Box mr="4" textAlign="right">
-        <Text>Hi, Lucas Flaquer</Text>
+        <Text>Hi, {user?.name}</Text>
       </Box>
 
-      <Avatar size="md" name="Lucas Flaquer" />
+      <Avatar size="md" name={user?.name} />
 
-      <Icon as={FaChevronDown} ml="2" />
+      {/* <Icon as={FaChevronDown}  /> */}
+      <Menu>
+        <MenuButton
+          ml="2"
+          as={IconButton}
+          icon={<Icon as={FaChevronDown} />}
+          variant="outline"
+          borderColor="transparent"
+          background="transparent"
+        />
+        <MenuList right="0">
+          <MenuItem>Meus Dados</MenuItem>
+          <MenuItem onClick={() => signOut()}>Sair</MenuItem>
+        </MenuList>
+      </Menu>
     </Flex>
   );
 }
