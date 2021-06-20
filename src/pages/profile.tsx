c86895118dca1107c61a686f37784989f9088f73
LucasFlaquer <lucas.flaquer@gmail.com>
2021-06-19 21:40:03 -0300

    profile screen

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,29 @@
-import { Box, Flex, Text, Avatar, Button, Icon } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Text,
+  Avatar,
+  Button,
+  Icon,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from '@chakra-ui/react';
+import { useRef } from 'react';
 import { FiEdit, FiUser } from 'react-icons/fi';
 import { BaseTemplate } from '../components/Templates/BaseLayout';
 import { useAuthContext } from '../contexts/AuthContex';
 
 export default function Profile() {
   const { user } = useAuthContext();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const finalRef = useRef();
+
   return (
     <BaseTemplate icon={FiUser} title="Meus Dados">
       <Box
@@ -23,12 +42,32 @@ export default function Profile() {
               <strong>Email:</strong> {user?.email}
             </Text>
           </Box>
-          <Button href="#" marginRight={6} marginTop={2} marginLeft="auto">
+          <Button
+            onClick={onOpen}
+            marginRight={6}
+            marginTop={2}
+            marginLeft="auto"
+          >
             <Icon as={FiEdit} marginRight={2} />
             Atualizar meus Dados
           </Button>
         </Flex>
       </Box>
+      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>FORM DE USUÀRIO</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </BaseTemplate>
   );
 }
